4bbdbdd3ef8dfb66cbc165612f87cbbccb836ac5
marayufotina <45465008+marayufotina@users.noreply.github.com>
2018-11-30 14:19:57 +0700

    Add files via upload

---
+++ SG2.m
@@ -0,0 +1,33 @@
+%Perbandingan angka
+
+angka1 = input ('masukkan angka A : ');
+angka2 = input ('masukkan angka B : ');
+if angka1 > angka2;
+    hasil = 'A lebih besar daipada B';
+else
+    hasil = 'B lebih besar daripada A';
+end
+disp (hasil);
+
+
+%Array 2 dimensi
+
+A = [4 6; 8 3]
+B = [4 3; 2 3]
+
+% Pertambahan
+Pertambahan = A + B, '';
+% Pengurangan
+Pengurangan = A - B, '';
+% Pembagian
+Pembagian = A / B, '';
+% Perkalian
+Perkalian = A*B, ''; 
+% Perpangkatan
+Perpangkatan = A.^B , '';
+%Perkalian elemen
+Elemen = A.*B, '';
+%Perkalian Skalar
+k = 2;
+Skalar = A*2, '';
+
